f9bd01a8a2b8aa418d93f3e8d3914dbea7c3e53f
Avneet Singh <avneet723@gmail.com>
2013-01-09 01:16:45 -0800

    Deleted the table put in for debugging

--- signup.php
+++ signup.php
@@ -102,64 +102,52 @@
         </div>
 
         <section class="span6">
-          <div id="Form1">
+          
           <form name="signup" method="post" action="signup_form_send.php">
-            <table>
-            <tr>
+            <fieldset>
+
             <div class="control-group">
-              <td>
               <label class="control-label" for="user_name"><i class="icon-user"></i>  Username</label>
-              </td>
               <div class="controls controls-row">
-                <td>
-                <input type="text" class="input-xxlarge" id="user_name" placeholder="Please enter your username">
-                </td>
+                <input type="text" class="input-xxlarge" id="user_name" placeholder="Please enter your email">
               </div>
             </div>
-            </tr>
 
-            <tr>
             <div class="control-group">
               <label class="control-label" for="full_name"><i class="icon-user"></i> Full Name</label>
               <div class="controls controls-row">
                 <input type="text" class="input-xxlarge" id="full_name" placeholder="Please enter your email">
               </div>
             </div>
-            </tr>
 
-            <tr>
             <div class="control-group">
               <label class="control-label" for="email"><i class="icon-envelope"></i> Email</label>
               <div class="controls controls-row">
                 <input type="text" class="input-xxlarge" id="email" placeholder="you@yourdomain.com">
               </div>
             </div>
-            </tr>
 
-            <tr>
             <div class="control-group">
               <label class="control-label" for="im_handle"><i class="icon-globe"></i> IM Handle</label>
               <div class="controls controls-row">
                 <input type="text" class="input-xxlarge" id="im_handle" placeholder="your_handle@your_messenger">
               </div>
             </div>
-            </tr>
 
-            <tr>
             <div class="control-group">
               <label class="control-label" for="phone_num"><i class="icon-comment"></i> Phone Number</label>
               <div class="controls controls-row">
                 <input type="text" class="input-xxlarge" id="phone_num" placeholder="555-555-5555">
               </div>
             </div>
-            </tr>
+
             <div class="control-group">
             <div class="controls">
               <input type="submit" class="btn btn-success" value="Sign Up">
             </div>
-          </table>
+            </fieldset>
           </form>
-        </div>
+
         </section>
         <section class="span6">
 
@@ -178,7 +166,6 @@
                   <button class="btn btn-large btn-primary" type="submit">Sign in</button>
                 </span>
               </form>
-
 
             </div> <!-- /container --> 
 
